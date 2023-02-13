7767b258c53f63ae9fcee3c30ab2c8f6ef7dbe81
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-13 22:02:50 +0530

    Added LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next; 
+};
+
+class LinkedList{
+    public:
+    LinkedList(){}
+    void addNode(){}
+};
+
+int main(){
+
+    return 0;
+}
