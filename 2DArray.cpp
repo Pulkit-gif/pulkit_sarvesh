4c5e6f570fbd9744dbcc91b1879267bdc9f2807b
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-13 21:44:47 +0530

    Update 2DArray.cpp

--- 2DArray.cpp
+++ 2DArray.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int ar[3][3] = {53,3,23,
+                    5,14,82,
+                    12,11,9};
+
+    cout<<"==> "<<ar[2][1]; 
     return 0;
 }
