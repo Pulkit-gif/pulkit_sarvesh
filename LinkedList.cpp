12c9f85def53af3d85a699b0b8a57ffb1ac7fdfb
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-16 20:38:01 +0530

    Updated LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -3,16 +3,59 @@ using namespace std;
 
 struct Node{
     int data;
-    Node* next; 
+    Node* next;
 };
 
 class LinkedList{
+    private:
+    Node *head = NULL;
+    int x;
     public:
     LinkedList(){}
-    void addNode(){}
+
+    void insertNode(int value){
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = nullptr;
+        if(isEmpty()){
+            head = newNode;
+        } else {
+            Node* temp = new Node();
+            temp = head;
+            while(temp->next){
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    void deleteNodeAtEnd(){
+        
+    }
+
+    bool isEmpty(){
+        if(head == NULL){
+            return true;
+        } else return false;
+    }
+    void display(){
+        Node* temp = new Node();
+        temp = head;
+        cout<<endl;
+        while(temp != nullptr){
+            cout<<"\t"<<temp->data;
+            temp = temp->next;
+        }
+    }
 };
 
 int main(){
-
+    LinkedList list;
+    list.insertNode(12);
+    list.insertNode(10);
+    list.insertNode(5);
+    list.insertNode(22);
+    list.insertNode(2);
+    list.insertNode(50);
+    list.display();
     return 0;
 }
