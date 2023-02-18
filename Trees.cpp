91ab9ef448cbdde7accb7db293eaf2149a247f39
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-18 20:35:01 +0530

    Created Trees.cpp

--- Trees.cpp
+++ Trees.cpp
@@ -14,17 +14,26 @@ class BSTree{
 
     BSTree(){ root = NULL; }
 
-    void addNode(int value){
+    Node* getNewNode(int val){
         Node* newNode = new Node();
-        newNode->data = value;
+        newNode->data = val;
         newNode->left = nullptr;
         newNode->right = nullptr;
+        return newNode;
+    }
 
-        if(isEmpty()){
-            root = newNode;
-        } else {
-            
-        }
+    Node *insert(Node* root, int val)
+    {
+        if(root == NULL)
+            return getNewNode(val);
+    
+        if(root->data < val)
+            root->right = insert(root->right,val);
+   
+        else if(root->data > val)
+            root->left = insert(root->left,val);
+
+        return root;
     }
 
     bool isEmpty(){
@@ -33,14 +42,39 @@ class BSTree{
         else return false;
     }
 
-    void inOrder(){}
-    void preOrder(){}
-    void postOrder(){}
+    void inOrder(Node* root){
+        if(isEmpty()){
+            cout<<"Tree is Empty";
+        } else {
+            inOrder(root->left);
+            cout<<"\t"<<root->data;
+            inOrder(root->right);
+        } 
+    }
+    void preOrder(){
+        if(isEmpty()){
+            cout<<"Tree is Empty";
+        } else {
+            cout<<"\t"<<root->data;
+            inOrder(root->left);
+            inOrder(root->right);
+        } 
+    }
+    void postOrder(){
+        if(isEmpty()){
+            cout<<"Tree is Empty";
+        } else {
+            inOrder(root->left);
+            inOrder(root->right);
+            cout<<"\t"<<root->data;
+        } 
+    }
 
     
 };
 
 int main(){
+    BSTree tree;
 
     return 0;
 }
