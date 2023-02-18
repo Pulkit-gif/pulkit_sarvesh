98c9fbecc9ed62c57ba93c0479e5c2459f9dc08b
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-18 21:28:36 +0530

    Created Graphs.cpp

--- Trees.cpp
+++ Trees.cpp
@@ -1,80 +1,85 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 };
 
-class BSTree{
-    private:
-    Node* root;
-    public:
+Node *getNewNode(int val)
+{
+    Node *newNode = new Node();
+    newNode->data = val;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    return newNode;
+}
 
-    BSTree(){ root = NULL; }
+Node *insert(Node *root, int val)
+{
+    if (root == NULL)
+        return getNewNode(val);
 
-    Node* getNewNode(int val){
-        Node* newNode = new Node();
-        newNode->data = val;
-        newNode->left = nullptr;
-        newNode->right = nullptr;
-        return newNode;
-    }
+    if (root->data < val)
+        root->right = insert(root->right, val);
 
-    Node *insert(Node* root, int val)
+    else if (root->data > val)
+        root->left = insert(root->left, val);
+
+    return root;
+}
+
+ void inOrder(Node *node)
+{
+     if (node == NULL)
     {
-        if(root == NULL)
-            return getNewNode(val);
+        cout << "{0}";
+    }
+    else
+    {
+        inOrder(node->left);
+        cout << "\t" << node->data;
+        inOrder(node->right);
+    }
+} 
+
+void preOrder(Node *node)
+{
+    if (node == NULL)
+    {
+        cout << "{0}";
+    }
+    else
+    {
+        cout << "\t" << node->data;
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+}
+void postOrder(Node *node)
+{
+    if (node == NULL)
+    {
+        cout << "{0}";
+    }
+    else
+    {
+        postOrder(node->left);
+        postOrder(node->right);
+        cout << "\t" << node->data;
+    }
+}
+
+int main()
+{
+
+    Node *root = getNewNode(100);
+
+    insert(root, 50);
+    insert(root, 150);
     
-        if(root->data < val)
-            root->right = insert(root->right,val);
-   
-        else if(root->data > val)
-            root->left = insert(root->left,val);
-
-        return root;
-    }
-
-    bool isEmpty(){
-        if(root == NULL)
-            return true;
-        else return false;
-    }
-
-    void inOrder(Node* root){
-        if(isEmpty()){
-            cout<<"Tree is Empty";
-        } else {
-            inOrder(root->left);
-            cout<<"\t"<<root->data;
-            inOrder(root->right);
-        } 
-    }
-    void preOrder(){
-        if(isEmpty()){
-            cout<<"Tree is Empty";
-        } else {
-            cout<<"\t"<<root->data;
-            inOrder(root->left);
-            inOrder(root->right);
-        } 
-    }
-    void postOrder(){
-        if(isEmpty()){
-            cout<<"Tree is Empty";
-        } else {
-            inOrder(root->left);
-            inOrder(root->right);
-            cout<<"\t"<<root->data;
-        } 
-    }
-
-    
-};
-
-int main(){
-    BSTree tree;
-
+    inOrder(root);
     return 0;
 }
