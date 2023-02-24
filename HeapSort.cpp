cb595225916c4783e0ad01ae3ebf66f28c5d03a2
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-24 21:09:51 +0530

    Updated InsertionSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 
+int A[] = {19, 11, 4, 14, 2, 57, 31};
+
 int main(){
     
     return 0;
--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,6 +2,15 @@
 #include<vector>
 using namespace std;
 
+int A[] = {19, 11, 4, 14, 2, 57, 31};
+
+void InsertionSort(int ar[],int size){
+    int i,j;
+    for(i = 0;i <= size;i++){
+        
+    }
+}
+
 int main(){
     
     return 0;
