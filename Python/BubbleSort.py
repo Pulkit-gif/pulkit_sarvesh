402ef153aeaf39736626182731718ecb670b0d62
Sarvesh <sarveshkumar10101@gmail.com>
2023-02-26 12:18:18 +0530

    Created BinarySearch.py

---
+++ Python/BubbleSort.py
@@ -0,0 +1,21 @@
+def swap(x,y):
+    x = x + y
+    y = x - y
+    x = x - y
+
+def BubbleSort(ar):
+    size = len(ar)
+    for x in range(size):
+        for y in range(size-x-1):
+            if ar[y] > ar[y+1]:
+                temp = ar[y]
+                ar[y] = ar[y+1]
+                ar[y+1] = temp
+
+def main():
+    ar = [12,77,98,45,8,5,44,36,15,50]
+    BubbleSort(ar)
+    print(ar)
+
+if __name__ == '__main__':
+    main()
